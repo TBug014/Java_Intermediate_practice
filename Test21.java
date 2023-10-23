9f29d214bace77e4d224c2aeb3f00f877cc1a1bd
TBug014 <parthapkayshop14@gmail.com>
2023-10-23 23:17:34 +0530

    runtime errors Exception handling

---
+++ Test21.java
@@ -0,0 +1,26 @@
+// Exceptions / these are runtime errors
+
+
+
+// runtime errors
+public class Test21 {
+    public static void main(String[] args) {
+
+        int i = 0;
+        int j = 0;
+
+        // Exception Handling
+        try
+        {
+            j = 18/i;
+
+        }
+        catch(Exception e){
+            System.out.println("Something Went wrong");
+        }
+
+        System.out.println(j);
+        System.out.println("Bye");
+
+    }
+}
