74b9145c6d25869f95f82520665a0892e143e8a0
TBug014 <parthapkayshop14@gmail.com>
2023-10-24 23:25:38 +0530

    Custom Exception

---
+++ Test24.java
@@ -0,0 +1,34 @@
+//Custom Exception 
+
+class ParthaException extends Exception{
+    public ParthaException(String string)
+    {
+        super(string);
+    }
+}
+
+public class Test24 {
+    public static void main(String[] args) {
+        int i = 0;
+        int j = 0;
+
+        try{
+            j = 18/i;
+            if (j == 0)
+                throw new ParthaException("I dont want to print zero");
+        }
+        catch(ParthaException e){
+            j = 18/1;
+            System.out.println("Thats the default output. ");
+        }
+      
+        catch(Exception e)
+        {
+            System.out.println("Something went wrong." + e);
+        }
+
+        System.out.println(j);
+
+    }
+}
+
