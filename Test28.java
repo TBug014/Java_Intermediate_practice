e9da98e664742fd32359e3e94023846694956a4a
TBug014 <parthapkayshop14@gmail.com>
2023-10-25 01:07:37 +0530

    Multiple threads in java

---
+++ Test28.java
@@ -0,0 +1,30 @@
+// Multiple threads in java
+
+class A extends Thread{
+    public void run(){
+        for(int i = 0;  i <=10; i++){
+        System.out.println("Hi");
+
+        }
+    }
+}
+class B extends Thread{
+    public void run(){
+        for(int i = 0;  i <=10; i++){
+        System.out.println("Hello");
+
+        }
+    }
+}
+public class Test28 {
+    public static void main(String[] args) {
+        
+        A obj1 = new A();
+        B obj2 = new B();
+
+        obj1.start();
+        obj2.start();
+
+
+    }
+}
