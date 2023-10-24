b6ca97d4e0434436ce61b2bea3680d4bb0dc2755
TBug014 <parthapkayshop14@gmail.com>
2023-10-25 00:22:57 +0530

    Try With Resources

---
+++ Test27.java
@@ -0,0 +1,20 @@
+// Try with resources
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Test27 {
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        
+        int num = 0;
+        try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))){
+            num = Integer.parseInt(br.readLine());
+            System.out.println(num);
+        }
+        // Whenever we want to close resourse we use finally 
+        // finally{
+        //     br.close();
+        // }
+    }
+}
