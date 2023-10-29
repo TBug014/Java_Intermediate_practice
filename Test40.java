269a94e2238460c263271fb96aa0182cd7f44f7c
TBug014 <parthapkayshop14@gmail.com>
2023-10-29 23:51:47 +0530

    Local Variable Type Inference

---
+++ Test40.java
@@ -0,0 +1,23 @@
+// Local Variable Type Inference
+
+
+public class Test40 {
+    public static void main(String[] args) {        
+        int a = 8;
+
+
+
+        // we can use var for all local variable
+        var b = 9;
+
+        String var = "Partha";
+        int c;
+        var d = 10;
+
+        int nums[] = new int[10];
+
+        var num = new int[100];
+
+        var obj = new part();
+    }
+}
